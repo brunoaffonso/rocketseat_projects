796446ac70830063daf05a49b94959c1d1b3f638
Bruno Affonso <brunoaffonso@gmail.com>
2025-08-30 13:38:16 -0300

    initial commit

---
+++ bookwise/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Book Wise</title>
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="bg-slate-800 text-slate-200">
+    <header class="bg-slate-700">
+        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+            <div class="font-bold text-xl">Book Wise</div>
+            <ul class="flex space-x-4 font-bold">
+                <li><a href="/" class="text-emerald-400">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+            </ul>
+            <ul>
+                <li><a href="/login.php">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <main class="mx-auto max-w-screen-lg">
+        lista de livros
+    </main>
+</body>
+
+</html>
