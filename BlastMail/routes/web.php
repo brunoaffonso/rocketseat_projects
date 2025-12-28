29c89bfc3073964b5c4c61cdb4034e5bac3ed570
Bruno Affonso <brunoaffonso@gmail.com>
2025-12-28 18:49:55 -0300

    feat: Implement email list management with new model, controller, migration, and views, and update the application's landing page.

--- BlastMail/routes/web.php
+++ BlastMail/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\EmailListController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'welcome');
+Route::view('/', 'home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -13,6 +14,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/email-list', [EmailListController::class, 'index'])->name('email-list.index');
+    Route::get('/email-list/create', [EmailListController::class, 'create'])->name('email-list.create');
 });
 
 require __DIR__.'/auth.php';
